0b3c0b83e4bc0aca0c88c4f6058b693fa2bb7659
allbkm <91130798+allbkm@users.noreply.github.com>
2025-11-05 08:33:30 +0400

    Update 1.5.sql

--- 1.5.sql
+++ 1.5.sql
@@ -1,2 +1,12 @@
-select name from tracks
-where name like '%My%' or name like '%мой%';
+SELECT name 
+FROM tracks
+WHERE 
+    name ~*\y(my|мой)\y' OR
+    name ILIKE 'my %' OR 
+    name ILIKE '% my' OR 
+    name ILIKE '% my %' OR
+    name ILIKE 'мой %' OR 
+    name ILIKE '% мой' OR 
+    name ILIKE '% мой %' OR
+    name ILIKE 'my' OR
+    name ILIKE 'мой';
